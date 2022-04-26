f0a4427ca7d7357712d5714436455ed1cbe1b3e2
Nurbol Abaikanov <Abaikanov@Nurbols-MacBook-Pro.local>
2022-04-26 19:01:46 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+
+
+    {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.next();
+
+    }
+
+    static String arr() {
+        return "a";
+    }
+
+
+
+
+}
